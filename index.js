c8152b680a94381d981f92a4acfcde65107a257d
Aymeric-Lancien <aymericlancien12@gmail.com>
2021-02-04 15:44:50 +0100

    post route

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (request, response) => {
   });
 });
 
-app.get("/players/infos/:id", (request, response) => {
+app.get("/infos/:id", (request, response) => {
   connection.query(
     "SELECT * FROM tracks t JOIN guitar_players gp ON gp.idguitar_players = t.guitar_players_idguitar_players JOIN bands b ON gp.bands_idbands = b.idbands WHERE gp.idguitar_players = ?",
     [request.params.id],
@@ -38,6 +38,23 @@ app.get("/players/infos/:id", (request, response) => {
         response.status(500).send(error);
       }
       if (result.lenght === 0) {
+        response.sendStatus(404);
+      } else {
+        response.status(200).json(result);
+      }
+    }
+  );
+});
+
+app.post("/newtrack", (request, response) => {
+  const { track_name, guitar_players_idguitar_players } = request.body;
+  connection.query(
+    "INSERT INTO tracks (track_name, guitar_players_idguitar_players) VALUES (?, ?)",
+    [track_name, guitar_players_idguitar_players],
+    (error, result) => {
+      if (error) {
+        response.status(500).send(error);
+      } else if (result.length === 0) {
         response.sendStatus(404);
       } else {
         response.status(200).json(result);
